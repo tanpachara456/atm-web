d94172d3bd65922689fcb21d72e48942f01f9ed0
tanpachara <tanpacharahsnl@gmail.com>
2020-09-20 21:37:06 +0700

    worknaja

---
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -0,0 +1,31 @@
+package th.ac.ku.atm.service;
+
+import org.springframework.stereotype.Service;
+import th.ac.ku.atm.model.BankAccount;
+import th.ac.ku.atm.model.Customer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BankAccountService {
+    private List<BankAccount> bankAccountList;
+
+    @PostConstruct
+    public void postConstruct(){
+        this.bankAccountList = new ArrayList<>();
+    }
+
+    public void createBankAccount(BankAccount bankAccount){
+        bankAccountList.add(bankAccount);
+    }
+
+    public List<BankAccount> getBankAccountList() {
+        return new ArrayList<>(this.bankAccountList);
+    }
+
+
+
+
+}
