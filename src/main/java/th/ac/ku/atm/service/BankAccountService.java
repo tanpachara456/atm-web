57a9958f2db9e7fb25d7c0aa72c1365724147995
tanpachara <tanpacharahsnl@gmail.com>
2020-10-04 20:11:11 +0700

    1.8.4

--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -29,6 +29,30 @@ public class BankAccountService {
 
         return Arrays.asList(accounts);
     }
+    public List<BankAccount> getBankAccounts(){
+        String url = "http://localhost:8091/api/bankaccount";
+        ResponseEntity<BankAccount[]> response =
+                restTemplate.getForEntity(url, BankAccount[].class);
+        BankAccount[] accounts = response.getBody();
+        return Arrays.asList(accounts);
+    }
 
+    public void openBankAccount(BankAccount bankAccount){
+        String url = "http://localhost:8091/api/bankaccount";
+        restTemplate.postForObject(url, bankAccount, BankAccount.class);
+    }
+
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8091/api/bankaccount" + id;
+        ResponseEntity<BankAccount> response =
+                restTemplate.getForEntity(url, BankAccount.class);
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount){
+        String url = "http://localhost:8091/api/bankaccount" +
+                                                    bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
 
 }
